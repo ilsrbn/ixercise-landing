fb3f1d5317c80a742138231b5739765240a77be0
ilsrbn <serbini271@gmail.com>
2026-04-29 14:06:29 +0300

    chore: configure umami website id

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 const siteUrl = (process.env.NUXT_PUBLIC_SITE_URL || 'https://www.ixercise.com').replace(/\/$/, '')
+const umamiWebsiteId = process.env.NUXT_PUBLIC_UMAMI_WEBSITE_ID || ''
 
 export default defineNuxtConfig({
   ssr: true,
@@ -30,13 +31,15 @@ export default defineNuxtConfig({
         { name: 'robots', content: 'index, follow' },
         { property: 'og:site_name', content: 'Ixercise' }
       ],
-      script: [
-        {
-          src: 'https://cloud.umami.is/script.js',
-          defer: true,
-          'data-website-id': '1311d774-7582-4a83-a3ef-0a7727699d6c'
-        }
-      ],
+      script: umamiWebsiteId
+        ? [
+            {
+              src: 'https://cloud.umami.is/script.js',
+              defer: true,
+              'data-website-id': umamiWebsiteId
+            }
+          ]
+        : [],
       link: [
         { rel: 'icon', href: '/favicon-32x32.png', type: 'image/png', sizes: '32x32' },
         { rel: 'icon', href: '/favicon.png', type: 'image/png', sizes: '64x64' },
@@ -56,7 +59,8 @@ export default defineNuxtConfig({
     waitlistFromEmail: process.env.WAITLIST_FROM_EMAIL || 'Ixercise Waitlist <onboarding@resend.dev>',
     public: {
       waitlistEndpoint: process.env.NUXT_PUBLIC_WAITLIST_ENDPOINT || '',
-      siteUrl
+      siteUrl,
+      umamiWebsiteId
     }
   },
   nitro: {
